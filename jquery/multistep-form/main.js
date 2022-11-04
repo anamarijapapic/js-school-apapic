d2479357385a03b70eaf4a2bb14542877d033f86
Anamarija Papic <anamarija.papic@agilo.co>
2022-11-04 14:33:32 +0100

    Multistep form - 01 (Validation)

--- jquery/multistep-form/main.js
+++ jquery/multistep-form/main.js
@@ -4,6 +4,34 @@
         $('.selectPropertyType01').select2();
         $('.selectPropertySize01').select2();
     });
+
+    function verifyZipCode() {
+        let zipCode = $('#propertyZip').val();
+        if (zipCode.length > 4 && zipCode.length < 13 && zipCode.match(/^\d+$/)) {
+            return true;
+        }
+        return false;
+    }
+
+    function verifyForm() {
+        if ($('.selectPropertyType01').val() != '' && $('.selectPropertySize01').val() != '' && verifyZipCode()) {
+            return true;
+        } 
+        return false;
+    }
+
+    function updateFormStatus() {
+        if (verifyForm()) {
+            $('.submit').removeClass('disabled');
+        } 
+        else {
+            $('.submit').addClass('disabled');
+        }
+    }
+    
+    $('.selectPropertyType01').change(updateFormStatus);
+    $('.selectPropertySize01').change(updateFormStatus);
+    $('#propertyZip').change(updateFormStatus);
 })();
 
 // Multistep form 02
