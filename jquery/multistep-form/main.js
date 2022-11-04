3502373e6c3c1f5cfe26eb0fdbab749f9603d2fe
Anamarija Papic <anamarija.papic@agilo.co>
2022-11-04 11:59:22 +0100

    Multistep forms - initialize Select2 plugin

---
+++ jquery/multistep-form/main.js
@@ -0,0 +1,15 @@
+// Multistep form 01
+(function () {
+    $(document).ready(function() {
+        $('.selectPropertyType01').select2();
+        $('.selectPropertySize01').select2();
+    });
+})();
+
+// Multistep form 02
+(function () {
+    $(document).ready(function() {
+        $('.selectPropertyType02').select2();
+        $('.selectPropertySize02').select2();
+    });
+})();
