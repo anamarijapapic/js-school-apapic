74321c50c2d34369bb7741e6481fa4d7e6a25aac
Anamarija Papic <anamarija.papic@agilo.co>
2022-11-10 14:02:05 +0100

    Form - Show feedback (success & errors)

--- jquery/progress-bar-validation-form/main.js
+++ jquery/progress-bar-validation-form/main.js
@@ -30,17 +30,43 @@
         return false;
     }
 
+    function showError(field) {
+        let formGroup = $(field).parent().parent();
+        let helpBlock = formGroup.find('.help-block');
+        let feedback = formGroup.find('.form-control-feedback');
+        formGroup.removeClass('has-success');
+        feedback.addClass('hidden');
+        formGroup.addClass('has-error');
+        helpBlock.removeClass('hidden');
+    }
+
+    function showSuccess(field) {
+        let formGroup = $(field).parent().parent();
+        let helpBlock = formGroup.find('.help-block');
+        let feedback = formGroup.find('.form-control-feedback');
+        formGroup.removeClass('has-error');
+        helpBlock.addClass('hidden');
+        formGroup.addClass('has-success');
+        feedback.removeClass('hidden');
+    }
+
     var valueTotal = 0;
     var validatedFields = {};
     var percent = (100 / $('input').filter('[required]').length);
     $('input').filter('[required]').on('input', function() {
         $('input').filter('[required]').each(function(index, element) {
-            if (validate($(element)) && ! validatedFields[index]) {
-                validatedFields[index] = true;
-                valueTotal += percent;
-            } else if (! validate($(element)) && validatedFields[index]) {
-                validatedFields[index] = false;
-                valueTotal -= percent;
+            if (! validate($(element))) {
+                showError(element);
+                if (validatedFields[index]) {
+                    validatedFields[index] = false;
+                    valueTotal -= percent;
+                }
+            } else {
+                showSuccess(element);
+                if (! validatedFields[index]) {
+                    validatedFields[index] = true;
+                    valueTotal += percent;
+                }
             }
         });
         $('.progress-text').html(valueTotal.toFixed(0) + '% complete');
