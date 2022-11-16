6142bd1b9f8ca21cf07d2fb488a6e61189306e59
Anamarija Papic <anamarija.papic@agilo.co>
2022-11-16 16:16:21 +0100

    Fixed section navigation - Create fixed nav menu

--- jquery/fixed-section-navigation/html/source/js/scripts.js
+++ jquery/fixed-section-navigation/html/source/js/scripts.js
@@ -1,5 +1,19 @@
 ( function( $ ) {
 	'use strict';
 
-	// Write your code here...
+	var navigationMenuLinks = document.querySelectorAll('.navigation-menu ul li');
+
+	window.addEventListener('scroll', function(event) {
+  	var fromTop = window.scrollY;
+
+		navigationMenuLinks.forEach(function(link) {
+			var section = document.querySelector(link.firstChild.hash);
+
+			if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+				link.classList.add('current');
+			} else {
+				link.classList.remove('current');
+			}
+		});
+	});
 }( jQuery ) );
